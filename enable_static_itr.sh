d0caf270ad872ce9c65a9eba4b8a90b1e28124d3
hand32 <hand32@server.hand32-158637.bayopsys-pg0.utah.cloudlab.us>
2023-06-06 14:50:28 -0600

    fix ethtool sudo

--- enable_static_itr.sh
+++ enable_static_itr.sh
@@ -2,11 +2,13 @@
 
 set -x
 
-sudo ethtool -c ens1f1np1
+sudo setcap cap_net_admin+ep /usr/sbin/ethtool
+
+ethtool -c ens1f1np1
 
 # disable dynamic ITR
-sudo ethtool -C ens1f1np1 adaptive-rx off adaptive-tx off
-sudo ethtool -C ens1f1np1 rx-usecs 2 tx-usecs 2
+ethtool -C ens1f1np1 adaptive-rx off adaptive-tx off
+ethtool -C ens1f1np1 rx-usecs 2 tx-usecs 2
 
 echo ""
-sudo ethtool -c ens1f1np1
+ethtool -c ens1f1np1
