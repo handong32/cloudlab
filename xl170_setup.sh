c5007b05383ab51f329e1aecb7ce742966e51c3f
hand32 <hand32@server.hand32-158637.bayopsys-pg0.utah.cloudlab.us>
2023-06-07 13:51:03 -0600

    add scipy

--- xl170_setup.sh
+++ xl170_setup.sh
@@ -3,10 +3,13 @@
 set -x
 
 sudo apt-get update
-sudo apt-get install -y cpufrequtils msr-tools netperf openjdk-8-jdk-headless libopencv-dev python3-opencv
+sudo apt-get install -y cpufrequtils msr-tools netperf openjdk-8-jdk-headless libopencv-dev python3-opencv python3-pip
 
 # update opencv symlink
 sudo ln -s /usr/lib/x86_64-linux-gnu/pkgconfig/opencv4.pc /usr/lib/x86_64-linux-gnu/pkgconfig/opencv.pc
+
+#install scipy
+pip install scipy
 
 # disable HT
 echo off | sudo tee /sys/devices/system/cpu/smt/control
