bd8f9100cee570acb6f7fd5119c892299601e426
hand32 <hand32@harness.hand32-160513.bayopsys-pg0.apt.emulab.net>
2023-06-25 18:39:13 -0600

    c6220

---
+++ c6220_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get update
+sudo apt-get install -y cpufrequtils msr-tools netperf openjdk-8-jdk-headless libopencv-dev python3-opencv python3-pip uuid-dev autotools-dev automake tcl libtool libreadline-dev libgtop2-dev bison swig scons libevent-dev gengetopt libzmq3-dev libevent-dev
+pip3 install ax-platform
+
+# update opencv symlink
+sudo ln -s /usr/lib/x86_64-linux-gnu/pkgconfig/opencv4.pc /usr/lib/x86_64-linux-gnu/pkgconfig/opencv.pc
+
+#install scipy
+pip install scipy
+
+# disable HT
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+
+# disable TurboBoost
+echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+
+# enable MSR
+sudo modprobe msr
+
+# dump cpu freq
+sudo rdmsr -a 0x199
+
+# open port
+sudo ufw allow 8080
+
+# actually, disable firewall
+sudo systemctl stop ufw
+
+# set irq affinity
+sudo ./set_irq_affinity.sh enp3s0f0
