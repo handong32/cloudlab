3335f32f2db6b63737a6826ad5ce93f449c92d2d
hand32 <hand32@harness.hand32-160612.bayopsys-pg0.utah.cloudlab.us>
2023-06-27 14:36:19 -0600

    0.3.3 of ax.dev limits choice parameters to < 1000

--- xl170_setup.sh
+++ xl170_setup.sh
@@ -5,7 +5,7 @@ set -x
 sudo apt-get update
 sudo apt-get install -y cpufrequtils msr-tools netperf openjdk-8-jdk-headless libopencv-dev python3-opencv python3-pip uuid-dev autotools-dev automake tcl libtool libreadline-dev libgtop2-dev bison swig scons libevent-dev gengetopt libzmq3-dev libevent-dev
 
-pip3 install ax-platform
+pip install ax-platform==0.3.2
 
 # update opencv symlink
 sudo ln -s /usr/lib/x86_64-linux-gnu/pkgconfig/opencv4.pc /usr/lib/x86_64-linux-gnu/pkgconfig/opencv.pc
