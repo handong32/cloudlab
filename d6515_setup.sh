c57b25acd92fbee915f4470598bddef2af794470
hand32 <hand32@server.hand32-160954.bayopsys-pg0.utah.cloudlab.us>
2023-07-05 15:49:56 -0600

    d6515 setup

---
+++ d6515_setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get update
+sudo apt-get install -y cpufrequtils msr-tools netperf openjdk-8-jdk-headless libopencv-dev python3-opencv python3-pip uuid-dev autotools-dev automake tcl libtool libreadline-dev libgtop2-dev bison swig scons libevent-dev gengetopt libzmq3-dev libevent-dev docker.io
+
+pip install ax-platform==0.3.2
+
+# update opencv symlink
+sudo ln -s /usr/lib/x86_64-linux-gnu/pkgconfig/opencv4.pc /usr/lib/x86_64-linux-gnu/pkgconfig/opencv.pc
+
+#install scipy
+pip install scipy
+
+# disable HT
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+
+# disable AMD Core Performance Boost (sounds like TurboBoost)
+sudo rdmsr 0xc0010015
+sudo wrmsr -a 0xc0010015 0x4b000011
+sudo rdmsr 0xc0010015
+
+# enable MSR
+sudo modprobe msr
+
+# amd energy
+sudo modprobe amd_energy
+
+# dump cpu freq
+#sudo rdmsr -a 0x199
+
+# open port
+sudo ufw allow 8080
+
+# actually, disable firewall
+sudo systemctl stop ufw
+
+# set irq affinity
+sudo killall irqbalance
+#ieth=((ifconfig | grep -B1 192.168.1 | grep -o "^\w*"))
+sudo ./set_irq_affinity.sh enp65s0f0np0
