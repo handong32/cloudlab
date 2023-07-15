ba1d5fa1c9e8519850bac60f565c3acae8f797dc
hand32 <hand32@server.hand32-160954.bayopsys-pg0.utah.cloudlab.us>
2023-07-15 15:09:08 -0600

    d6515 itr

--- set_itr.sh
+++ set_itr.sh
@@ -48,4 +48,19 @@ function dynamic_xl170
     ethtool -c ${ieth}
 }
 
+function dynamic_d6515
+{
+    sudo setcap cap_net_admin+ep /usr/sbin/ethtool
+    
+    ieth=$(ifconfig | grep -B1 192.168.1 | grep -o "^\w*")
+    ethtool -c ${ieth}
+    
+    # enable dynamic ITR
+    ethtool -C ${ieth} rx-usecs 8 tx-usecs 8 rx-frames 128 tx-frames 128 
+    ethtool -C ${ieth} adaptive-rx on adaptive-tx on
+    
+    echo ""
+    ethtool -c ${ieth}
+}
+
 "$@"
