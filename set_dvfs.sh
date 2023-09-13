e8f087bf41ee25168090080b0085cfe4deaea328
hand32 <handong@bu.edu>
2023-09-13 15:20:59 -0600

    update

--- set_dvfs.sh
+++ set_dvfs.sh
@@ -4,7 +4,7 @@ set -x
 
 function static
 {
-    sudo ./cpufreq-set-all -g userspace
+    sudo ~/cloudlab/cpufreq-set-all -g userspace
 
     sudo cpufreq-info
 
@@ -15,31 +15,31 @@ function static
 # conservative, ondemand, userspace, powersave, performance, schedutil 
 function ondemand
 {
-    sudo ./cpufreq-set-all -g ondemand
+    sudo ~/cloudlab/cpufreq-set-all -g ondemand
     sudo cpufreq-info
 }
 
 function conservative
 {
-    sudo ./cpufreq-set-all -g conservative
+    sudo ~/cloudlab/cpufreq-set-all -g conservative
     sudo cpufreq-info
 }
 
 function powersave
 {
-    sudo ./cpufreq-set-all -g powersave
+    sudo ~/cloudlab/cpufreq-set-all -g powersave
     sudo cpufreq-info
 }
 
 function performance
 {
-    sudo ./cpufreq-set-all -g performance
+    sudo ~/cloudlab/cpufreq-set-all -g performance
     sudo cpufreq-info
 }
 
 function schedutil
 {
-    sudo ./cpufreq-set-all -g schedutil
+    sudo ~/cloudlab/cpufreq-set-all -g schedutil
     sudo cpufreq-info
 }
 
