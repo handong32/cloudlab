ec65876bd1d1beed28d201cac63a7f5c3c04c7db
hand32 <handong@bu.edu>
2023-09-13 16:51:20 -0600

    fix a bug

--- getStats.sh
+++ getStats.sh
@@ -13,10 +13,10 @@ TXB=0
 for (( c=0; c<$(nproc); c++ ))
 do    
     POLL=$((POLL + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state0/usage)))
-    C1=$((POLL + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state1/usage)))
-    C1E=$((POLL + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state2/usage)))
-    C3=$((POLL + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state3/usage)))
-    C6=$((POLL + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state4/usage)))
+    C1=$((C1 + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state1/usage)))
+    C1E=$((C1E + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state2/usage)))
+    C3=$((C3 + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state3/usage)))
+    C6=$((C6 + $(cat /sys/devices/system/cpu/cpu${c}/cpuidle/state4/usage)))
 done
 
 tmp=$(ifconfig | grep -A5 10.10.1 | grep 'RX packets' | grep '\w*')
